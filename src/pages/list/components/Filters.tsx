ac7bb6407c9752e93bc63916077e2030c495f062
AmrahGarat <159684461+AmrahGarat@users.noreply.github.com>
2025-01-29 21:18:51 +0400

    feat(auth): add list page sidefilter

--- src/pages/list/components/Filters.tsx
+++ src/pages/list/components/Filters.tsx
@@ -1,18 +1,31 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react"
+import { useRef, useState } from "react"
 import FilterIcon from "@/assets/icons/filter.svg";
+import { cn } from "@/lib/utils";
+import { useOnClickOutside } from "usehooks-ts";
 
 
 export const Filters = () => {
-  const [isOpen,setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null)
+
+  function toggle () {
+    setIsOpen(!isOpen);
+  }
+  function handleClose() {
+    setIsOpen(false);
+  }
+  
+  useOnClickOutside (ref, handleClose)
+
   return (
-    <div>
-      <div className="hidden xl:block bg-white w-[360px] h-[calc(100vh-142px)] fixed top-[142px]">
-        <Filters/>
+    <>
+      <div ref={ref} className={cn("bg-white w-[360px] h-[calc(100vh-80px)] md:h-[calc(100vh-142px)] fixed top-[80px] md:top-[142px] z-20 duration-200", isOpen ? "left-0" : "-left-[360px] xl:left-0")}>
+        Filters
       </div>
-      <Button className="xl:hidden">
-        <img src={FilterIcon} alt="Filter" />
+      <Button variant={"outline"} onClick={toggle} className="xl:hidden w-fit ml-6 lg:ml-8 mt-4 -mb-4">
+        <img className="w-6 lg:w-8" src={FilterIcon} alt="Filter" />
       </Button>
-    </div>
+    </>
   )
 }
