747147802209100ff8fa00d26434fabd3de4c1be
AmrahGarat <159684461+AmrahGarat@users.noreply.github.com>
2025-02-10 19:52:58 +0400

    feat(backend): rent controller updated

--- server/src/controllers/rent.ts
+++ server/src/controllers/rent.ts
@@ -2,12 +2,14 @@ import { Request,Response } from "express";
 
 const getAll = async (req: Request, res: Response) => {
     const data = req.matchedData;
-
     res.json({
         data,
     })
 }
 
+const getById = async(req:Request,res:Response)=>{}
+
 export default {
     getAll,
+    getById
 }
